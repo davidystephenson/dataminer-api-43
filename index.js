c491f9af1d4676c4dba3a4446be4f0477a96b245
David Y. Stephenson <david@davidystephenson.com>
2020-07-17 14:11:30 +0200

    Registered general and user specific endpoints

---
+++ index.js
@@ -0,0 +1,37 @@
+const data = require('./data.json')
+const express = require('express')
+
+console.log('data test:', data)
+
+const app = express()
+
+const port = 3000
+
+function onListen () {
+  const message = `Listening on :${port}`
+
+  console.log(message)
+}
+
+app.get('/', function (request, response) {
+  response.send(data)
+})
+
+app.get(
+  '/user/:username',
+  (request, response) => {
+    const { username } = request.params
+
+    const user = data
+      .find(user => user.name === username)
+
+    response.send(user)
+  }
+)
+
+app.listen(port, onListen)
+
+// JSON API with Express
+// Two endpoints
+// One endpoint that returns all user data
+// Another endpoint that takes a user name parameter and returns the data for only that user
